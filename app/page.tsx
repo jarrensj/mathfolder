3d2f4b2d7f1a407a9331683725072c72b96da63a
Jarren San Jose <sanjose.jarren@gmail.com>
2024-02-25 15:36:19 -0800

    resize header text

--- app/page.tsx
+++ app/page.tsx
@@ -5,14 +5,14 @@ export default function Home() {
   return (
     <div className="flex flex-col items-center min-h-screen p-24">
       <main className="flex flex-col items-center flex-grow justify-center">
-        <h1 className="text-4xl font-bold">math folder</h1>
+        <h1 className="text-3xl font-bold">math folder</h1>
         <p className="text-lg mt-4">
           Check back soon!
         </p>
       </main>
       <footer className="mt-auto py-8">
         <p>
-          contact{" "}
+          code by{" "}
           <Link href="https://kwaji.com" target="_blank" className="text-blue-500 hover:underline">
               kwaji
           </Link>
