ffdbd027e6133dd0687270504803d8f5d9d44faf
Jarren San Jose <sanjose.jarren@gmail.com>
2024-02-25 15:30:33 -0800

    make landing page, check back soon

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,23 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center p-24">
-      <h1 className="text-4xl font-bold">math folder</h1>
-    </main>
+    <div className="flex flex-col items-center min-h-screen p-24">
+      <main className="flex flex-col items-center flex-grow justify-center">
+        <h1 className="text-4xl font-bold">math folder</h1>
+        <p className="text-lg mt-4">
+          Check back soon!
+        </p>
+      </main>
+      <footer className="mt-auto py-8">
+        <p>
+          contact{" "}
+          <Link href="https://kwaji.com" target="_blank" className="text-blue-500 hover:underline">
+              kwaji
+          </Link>
+        </p>
+      </footer>
+    </div>
   );
 }
